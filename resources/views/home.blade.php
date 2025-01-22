9c5bc4248c525ed9e321233778f4d1a227980eb3
Igiz <igizstarkle@gmail.com>
2025-01-22 19:26:19 +0300

    Passing data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,4 +3,8 @@
         Home Page
     </x-slot:heading>
     <h1>This is the Home Page</h1>
+
+    @foreach ($jobs as $job)
+    <li> <strong>{{ $job['title'] }}</strong>: earns {{ $job['salary'] }} </li>
+    @endforeach
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        'jobs' => [
+            [
+                'title' => 'PHP Developer',
+                'salary'   => '$60,000',
+            ],
+            [
+                'title' => 'Python Developer',
+                'salary'   => '$70,000',
+            ],
+            [
+                'title' => 'Java Developer',
+                'salary'   => '$80,000',
+            ],
+    ]]);
 });
 
 Route::get('/about', function () {
